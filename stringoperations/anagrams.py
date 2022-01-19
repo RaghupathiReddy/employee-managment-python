fb19367ea132141a8b9719e73ec0540d0864d340
ggk-rallapuram <raghupathi.allapuram@ggktech.com>
2022-01-19 16:04:45 +0530

    Added anagrams code

--- stringoperations/anagrams.py
+++ stringoperations/anagrams.py
@@ -1,12 +1,38 @@
 from collections import defaultdict
 
-def anagramList(sentence):
-    words = sentence.split(' ')
-    dict = defaultdict(list)
+
+def get_unique_list(anagram_list, word):
+    if anagram_list.count(word) == 0:
+        anagram_list.append(word)
+    return anagram_list
+
+
+def is_anagrams(words):
+    anagrams_set = map(lambda word: ''.join(sorted(word)), words)
+    if len(set(anagrams_set)) == 1:
+        return True
+    return False
+
+
+def get_anagrams(words):
+    anagram_dict = defaultdict(list)
     for word in words:
-        dict[str(sorted(word.lower()))].append(word)
+        anagram_dict[str(sorted(word.lower()))] = get_unique_list(anagram_dict[str(sorted(word.lower()))], word)
     result = []
-    for value in dict.values():
-        if(len(value) > 1):
-            result.append(value)
+    for value in anagram_dict.values():
+        if len(value) > 1:
+            if is_anagrams(value):
+                result.append(value)
     return result
+
+
+def main():
+    file = open('sentence.txt', 'r')
+    sentence = file.read()
+    words = sentence.split(' ')
+    anagrams_list = get_anagrams(words)
+    print(anagrams_list)
+
+
+if __name__ == "__main__":
+    main()
--- stringoperations/tests.py
+++ stringoperations/tests.py
@@ -6,6 +6,30 @@ import stringoperations.anagrams
 # Create your tests here.
 
 class Stringtestcases(unittest.TestCase):
-    def test_anagram_success_case(self):
-        result = stringoperations.anagrams.anagramList('test and Dan thing night')
-        self.assertEqual(result, [['and', 'Dan'], ['thing', 'night']])
+    def test_anagram_list(self):
+        words = ['this', 'is', 'a', 'real', 'tea', 'sentence', 'thing', 'ate', 'dusty', 'night', 'study', 'eat', 'tea']
+        result = stringoperations.anagrams.get_anagrams(words)
+        self.assertEqual(result, [['tea', 'ate', 'eat'], ['thing', 'night'], ['dusty', 'study']])
+
+    def test_get_unique_list_will_not_add_element_if_element_is_present_in_list(self):
+        dummy_list = ['test', 'check']
+        result = stringoperations.anagrams.get_unique_list(dummy_list, 'test')
+        self.assertEqual(result, ['test', 'check'])
+
+    def test_get_unique_list_will_add_element_if_element_is_not_present_in_list(self):
+        dummy_list = ['test', 'check']
+        result = stringoperations.anagrams.get_unique_list(dummy_list, 'test1')
+        self.assertEqual(result, ['test', 'check', 'test1'])
+
+    def test_is_anagrams_return_true_if_the_list_consists_of_anagrams(self):
+        words = ['thing', 'night']
+        result = stringoperations.anagrams.is_anagrams(words)
+        self.assertEqual(result, True)
+
+    def test_is_anagrams_return_false_if_the_list_does_not_consists_of_anagrams(self):
+        words = ['think', 'night']
+        result = stringoperations.anagrams.is_anagrams(words)
+        self.assertEqual(result, False)
+
+
+
